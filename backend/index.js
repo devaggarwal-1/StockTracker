137be637aba812f5d112448177025f25d54b496f
devaggarwal-1 <devaggarwal190@gmail.com>
2024-01-07 21:17:18 -0600

    axios changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const CustomerModel = require('./models/customer')
 const cors = require('cors')
 const corsOptions = {
-    origin: '/',
+    origin: 'https://stock-tracker-o6f622eg5-devaggarwal-1.vercel.app/',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
